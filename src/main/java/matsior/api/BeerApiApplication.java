1b94bd610bb2bbe5ad6bb86b9980076ad85ef3dc
Mateusz Krajewski <maatsior@gmail.com>
2022-06-27 10:21:11 +0200

    optimize imports

--- src/main/java/matsior/api/BeerApiApplication.java
+++ src/main/java/matsior/api/BeerApiApplication.java
@@ -1,12 +1,7 @@
 package matsior.api;
 
-import matsior.api.beer.Beer;
-import matsior.api.beer.dto.BeerDto;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BeerApiApplication {
