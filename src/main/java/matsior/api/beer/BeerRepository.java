77c86c86c5010ae5ae2292fb2809347fd401bba6
Mateusz Krajewski <maatsior@gmail.com>
2022-06-27 14:55:02 +0200

    add PagingAndSortingRepository

--- src/main/java/matsior/api/beer/BeerRepository.java
+++ src/main/java/matsior/api/beer/BeerRepository.java
@@ -1,10 +1,10 @@
 package matsior.api.beer;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface BeerRepository extends CrudRepository<Beer, Long> {
+public interface BeerRepository extends PagingAndSortingRepository<Beer, Long> {
     List<Beer> findAll();
     List<Beer> findAllByCountryIgnoreCase(String country);
     List<Beer> findAllByAlcoholGreaterThanEqual(double alcohol);
