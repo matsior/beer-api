81098e00e0b7f9c43ca4e7c431fb34c87d9ad5f0
Mateusz Krajewski <maatsior@gmail.com>
2022-06-29 17:44:46 +0200

    change swagger version to 3.0

--- src/main/java/matsior/api/BeerApiApplication.java
+++ src/main/java/matsior/api/BeerApiApplication.java
@@ -1,11 +1,12 @@
 package matsior.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Beer API", version = "0.0.1", description = "api providing beer info"))
 public class BeerApiApplication {
 
     public static void main(String[] args) {
