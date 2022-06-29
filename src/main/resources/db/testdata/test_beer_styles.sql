f5f6b307f65f0d7ad57f32cdf42beae90f3c34bb
Mateusz Krajewski <maatsior@gmail.com>
2022-06-29 16:42:00 +0200

    add developer profile

---
+++ src/main/resources/db/testdata/test_beer_styles.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+    beer_style(name, description)
+VALUES
+    ('Pale Lager', 'Very pale-to-golden-colored lager beer with a well-attenuated body and a varying degree of noble hop bitterness.'),
+    ('Witbier', 'Tradycyjne, belgijskie piwo pszeniczne zwane również biere blanche. Witbier jest warzony metodą górnej fermentacji i niefiltrowany, a ze względu na dodatek niesłodowanej pszenicy może być bardzo mętny.'),
+    ('Dortmunder Export', 'Jasny lager warzony pierwotnie przez Dortmunder Union w Dortmundzie w Niemczech w 1873 roku. Jest to piwo o miękkiej teksturze, inspirowane piwem Pilsner warzonym w Pilźnie.');
---
+++ src/main/resources/db/testdata/test_producers.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+    producer(name, description)
+VALUES
+    ('browar1', 'opis1'),
+    ('browar2', 'opis2'),
+    ('browar3', 'opis3');
