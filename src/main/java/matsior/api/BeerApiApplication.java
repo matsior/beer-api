84a282331a49f6dbf61d0d479fb005be6623a33c
Mateusz Krajewski <maatsior@gmail.com>
2022-07-12 12:18:26 +0200

    add SwagerConfig.java

--- src/main/java/matsior/api/BeerApiApplication.java
+++ src/main/java/matsior/api/BeerApiApplication.java
@@ -1,13 +1,10 @@
 package matsior.api;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Beer API", version = "0.0.1", description = "api providing beer info"))
 @EnableJpaAuditing
 public class BeerApiApplication {
 
---
+++ src/main/java/matsior/api/utils/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package matsior.api.utils;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Beer API", version = "0.0.1", description = "api providing beer info"))
+public class SwaggerConfig {
+}
